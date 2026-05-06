c82e658e705ed270f8b24dc1d4579e29848a101f
Eliezer Cavanha <contatoeliezer.cav@gmail.com>
2026-05-06 10:43:32 -0300

    UI: Refine Tecnologia Fluxa arc text positioning and sizing

--- src/components/DecorativeCircles.tsx
+++ src/components/DecorativeCircles.tsx
@@ -15,10 +15,10 @@ export default function DecorativeCircles() {
       {/* Texto em arco: tecnologia fluxa */}
       <div className="arc-text-container">
         <svg viewBox="0 0 400 400" width="400" height="400" className="arc-svg">
-          {/* Radius ~170px to accompany the 320px red circle edge */}
-          <path id="curve" d="M 40 200 A 160 160 0 0 1 360 200" fill="transparent" />
+          {/* Radius 165px to stay just outside the 320px red circle */}
+          <path id="curve" d="M 35 200 A 165 165 0 0 1 365 200" fill="transparent" />
           <text>
-            <textPath href="#curve" startOffset="75%" textAnchor="middle" className="arc-text">
+            <textPath href="#curve" startOffset="50%" textAnchor="middle" className="arc-text">
               tecnologia fluxa
             </textPath>
           </text>
